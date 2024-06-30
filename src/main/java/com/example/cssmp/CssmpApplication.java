0fcebc5df205f6883d4f32bdfda1506a2f7b752b
revan <revan_chan@163.com>
2024-06-30 20:09:22 +0800

    初始化项目

---
+++ src/main/java/com/example/cssmp/CssmpApplication.java
@@ -0,0 +1,13 @@
+package com.example.cssmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CssmpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CssmpApplication.class, args);
+    }
+
+}
